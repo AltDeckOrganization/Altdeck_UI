f58351fa64ba1e529d7bd9b9e3c0c587fe030b07
Joboa <joboa2015@gmail.com>
2022-05-24 19:09:09 +0000

    add recaptcha

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React from 'react';
 import './App.css';
 import Routing from './routing/Routing';
+import ReCAPTCHA from 'react-google-recaptcha'
+import axios from 'axios';
+const serverUrl = process.env.REACT_APP_BACKEND_URL;
 
 function App() {
+  const [captchaResult, setCaptchaResult] = React.useState()
+  const siteKey =  process.env.REACT_APP_RECAPTCHA_SITE_KEY
+
+  function onChange(value) {
+    // console.log('Captcha value:', value);
+    axios.get(`${serverUrl}/recaptcha`, {
+      method: 'POST',
+      body: JSON.stringify({ 'captcha_value': value }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.captcha.success)
+        setCaptchaResult(data.captcha.success)
+      })
+  }
   return (
     <div className="App">
+      <ReCAPTCHA
+        sitekey={siteKey}
+        onChange={onChange}
+      />
+      {
+        captchaResult && <button type='submit'>Submit</button>
+      }
       <Routing />
     </div>
   );
--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -7,12 +7,15 @@ import Home from "../pages/home/Home";
 import PromotionPage from "../pages/promotionPage/PromotionPage";
 import Tos from "../pages/tos/Tos";
 import PrivacyPolicy from "../pages/privacyPolicy/PrivacyPolicy";
+import TokenDetailPage from "../pages/home/coins/TokenDetailPage";
+
 export default function Routing() {
   return (
     <Router>
       <Header />
       <div>
         <Routes>
+          <Route path="/tokens/:id" element={TokenDetailPage} />
           <Route exact path="/" element={<Home />} />
           <Route path="/promotionPage" element={<PromotionPage />} />
           <Route path="/addNewToken" element={<AddNewToken />} />
